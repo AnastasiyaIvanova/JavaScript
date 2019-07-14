0018d46c88bfd8e7335b3c0d5fd688739329fe44
Nastya <nastasya_ivanova_2015@mail.ru>
2019-07-14 21:38:09 +0400

    task films

---
+++ task/films.js
@@ -0,0 +1,213 @@
+const films = [
+  {
+    title: 'The Green Mile',
+    creationYear: 1999,
+    country: ['USA'],
+    budget: '$60 000 000',
+    actors: [
+      {
+        name: 'Tom Hanks',
+        age: 63,
+        role: 'Paul Edgecomb',
+      },
+      {
+        name: 'David Morse',
+        age: 65,
+        role: 'Brutus Howell',
+      },
+      {
+        name: 'Michael Clarke Duncan',
+        age: 54,
+        role: 'John Coffey',
+      },
+    ],
+    director: {
+      name: 'Frank Darabont',
+      age: 60,
+      oscarsCount: 0,
+    }
+  },
+  {
+    title: 'Inception',
+    creationYear: 2010,
+    country: ['USA'],
+    budget: '$160 000 000',
+    actors: [
+      {
+        name: 'Leonardo DiCaprio',
+        age: 44,
+        role: 'Cobb',
+      },
+      {
+        name: 'Joseph Gordon-Levitt',
+        age: 38,
+        role: 'Arthur',
+      },
+      {
+        name: 'Ellen Page',
+        age: 32,
+        role: 'Ariadne',
+      },
+      {
+        name: 'Tom Hardy',
+        age: 41,
+        role: 'Eames',
+      },
+    ],
+    director: {
+      name: 'Christopher Nolan',
+      age: 48,
+      oscarsCount: 0,
+    }
+  },
+  {
+    title: 'Forrest Gump',
+    creationYear: 1994,
+    country: ['USA'],
+    budget: '$55 000 000',
+    actors: [
+      {
+        name: 'Tom Hanks',
+        age: 63,
+        role: 'Forrest Gump',
+      },
+      {
+        name: 'Robin Wright',
+        age: 53,
+        role: 'Jenny Curran',
+      },
+      {
+        name: 'Sally Field',
+        age: 72,
+        role: 'Mrs. Gump',
+      },
+    ],
+    director: {
+      name: 'Robert Zemeckis',
+      age: 68,
+      oscarsCount: 1,
+    }
+  },
+  {
+    title: 'Interstellar',
+    creationYear: 2014,
+    budget: '$165 000 000',
+    country: ['USA','Great Britain', 'Canada'],
+    actors: [
+      {
+        name: 'Matthew McConaughey',
+        age: 49,
+        role: 'Cooper',
+      },
+      {
+        name: 'Anne Hathaway',
+        age: 36,
+        role: 'Brand',
+      },
+      {
+        name: 'Jessica Chastain',
+        age: 42,
+        role: 'Murph',
+      },
+      {
+        name: 'Michael Caine',
+        age: 86,
+        role: 'Professor Brand',
+      },
+      {
+        name: 'Matt Damon',
+        age: 48,
+        role: 'Mann',
+      },
+    ],
+    director: {
+      name: 'Christopher Nolan',
+      age: 48,
+      oscarsCount: 0,
+    }
+  },
+  {
+    title: 'Catch Me If You Can',
+    creationYear: 2002,
+    budget: '$52 000 000',
+    country: ['USA', 'Canada'],
+    actors: [
+      {
+        name: 'Tom Hanks',
+        age: 63,
+        role: 'Carl Hanratty',
+      },
+      {
+        name: 'Leonardo DiCaprio',
+        age: 36,
+        role: 'Frank Abagnale Jr.',
+      },
+      {
+        name: 'Christopher Walken',
+        age: 76,
+        role: 'Frank Abagnale',
+      },
+      {
+        name: 'Amy Adams',
+        age: 44,
+        role: 'Brenda Strong',
+      },
+    ],
+    director: {
+      name: 'Steven Spielberg',
+      age: 72,
+      oscarsCount: 4,
+    }
+  },
+];
+
+//первое задание
+let countOfActors = 0;
+let sumAge = 0;
+films.forEach(function(item, i){
+  if (item.director.oscarsCount == 0){
+    countOfActors += item.actors.length;
+    const result = item.actors.reduce(function(sum, current){
+      return sum + current.age;
+    }, 0);
+    sumAge += result;
+  }
+});
+console.log(Math.round(sumAge / countOfActors));
+
+//второе задание
+let actors = [];
+let actor = [];
+films.forEach(function(item, i){
+  if (item.creationYear > 1995){
+    item.actors.forEach(function(item, i){
+      actors[i] = item.name;
+    });
+
+    if (actors.some(isTom)){
+      actors.splice(actors.indexOf('Tom Hanks'), 1)
+      actor = actor.concat(actors);
+    }
+
+    actors = [];
+  }
+});console.log(actor);
+
+function isTom(name) {
+  return name == 'Tom Hanks';
+}
+
+//третье задание
+let sum = 0;
+let actors2 = [];
+films.forEach(function(item, i){
+  if (item.director.age < 70){
+    item.actors.forEach(function(item, i){
+      actors2[i] = item.name;
+    });
+    if (!actors2.some(isTom)){
+      sum += parseInt(item.budget.substring(1, item.budget.length).replace(/\s/g, ''));
+    }
+  }
+});
+console.log(sum);
